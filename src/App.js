029f1d8824459e8c25fd4aca440bcd54b71c2156
Kyle Potter <kp1289@gmail.com>
2020-10-25 18:42:10 +0000

    created accordion and passing props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Accordion from './components/Accordion'      
+
+
+const items = [
+    {
+        title: 'What is React',
+        content: 'JS Framework'
+    },
+    {
+        title: 'Why use React',
+        content: 'Quick to learn and reap the benefits'
+    },
+    {
+        title: 'How do use react',
+        content: 'You build components'
+    }
+]
 
 export default () => {
     return (
         <div>
-            <h1>Widgets App</h1>
+            <Accordion items={items} />
         </div>
     )
 }
---
+++ src/components/Accordion.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Accordion({items}) {
+    return (
+        <div>
+            <h1>{items.length}</h1>
+        </div>
+    )
+}
+
+
+export default Accordion
