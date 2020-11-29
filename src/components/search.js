4ac61dede05ee3459f1c086d7da46918436d3e6c
Kyle Potter <kp1289@gmail.com>
2020-11-29 19:06:17 +0000

    calling wiki api

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function search() {
-    return (
-        <div>
-            <h1>Search</h1>
+const Search = () => {
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+
+  useEffect(() => {
+    const getWikipediaResponse = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      getWikipediaResponse();
+    }
+  }, [term]);
+
+  return (
+    <div>
+      <div className='ui form'>
+        <div className='field'>
+          <label htmlFor=''>Search</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className='input'
+            type='text'
+          />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default search
+export default Search;
