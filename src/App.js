bc1a2b4dab6ed962813ecc6e1e69f0e8b7804d62
Kyle Potter <kp1289@gmail.com>
2020-11-29 22:57:07 +0000

    added dropdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 // import Accordion from './components/Accordion';
 import Search from './components/search';
+import Dropdown from './components/Dropdown';
 
-// const items = [
-//   {
-//     title: 'What is React',
-//     content: 'JS Framework',
-//   },
-//   {
-//     title: 'Why use React',
-//     content: 'Quick to learn and reap the benefits',
-//   },
-//   {
-//     title: 'How do use react',
-//     content: 'You build components',
-//   },
-// ];
+const options = [
+  {
+    label: 'The Colour Red',
+    value: 'red',
+  },
+  {
+    label: 'The Colour Green',
+    value: 'green',
+  },
+  {
+    label: 'The Colour Blue',
+    value: 'blue',
+  },
+];
 
 export default () => {
   return (
     <div>
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
 };
--- src/components/search.js
+++ src/components/search.js
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('API');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const getWikipediaResponse = async () => {
@@ -13,27 +24,17 @@ const Search = () => {
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
 
       setResults(data.query.search);
     };
 
-    if (term && !results.length) {
+    if (debouncedTerm) {
       getWikipediaResponse();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          getWikipediaResponse();
-        }
-      }, 500);
-
-      return () => {
-        clearInterval(timeoutId);
-      };
     }
-  }, [term]);
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
